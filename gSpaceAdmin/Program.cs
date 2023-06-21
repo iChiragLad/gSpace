8bba5eb16585a5f8d26a29ba3bd3ddeb6786db1f
iChiragLad <chiragladn92@gmail.com>
2023-06-21 20:41:26 +0530

    implemented server streaming scenario for gSpaceAdmin

---
+++ gSpaceAdmin/Program.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using gSpaceAdmin.Protos;
+
+namespace gSpaceAdmin;
+class Program
+{
+  static async Task Main(string[] args)
+  {
+    var channel = GrpcChannel.ForAddress("http://localhost:5078", new GrpcChannelOptions { UnsafeUseInsecureChannelCallCredentials = true});
+    var client = new Gspace.GspaceClient(channel);
+
+    using var callStream = client.MonitorSpace(new Empty());
+
+    await foreach(var linemessage in callStream.ResponseStream.ReadAllAsync())
+    {
+      Console.WriteLine($"At : {linemessage.LinemessageTime}, received : {linemessage.LinemessageItem}");
+    }
+  }
+}
--- gSpaceServer/Services/GspaceService.cs
+++ gSpaceServer/Services/GspaceService.cs
@@ -1,5 +1,7 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using gSpaceServer.Protos;
+using System.Collections.ObjectModel;
 using static gSpaceServer.Protos.Gspace;
 
 namespace gSpaceServer.Services
@@ -16,7 +18,7 @@ namespace gSpaceServer.Services
     public override Task<RegistrationResponse> RegisterToSpace(RegistrationRequest request, ServerCallContext context)
     {
       _logger.LogInformation("Registring the user to the space...");
-      return Task.FromResult(new RegistrationResponse { SpaceId = Guid.NewGuid().ToString()});
+      return Task.FromResult(new RegistrationResponse { SpaceId = Guid.NewGuid().ToString() });
     }
 
     public override async Task<NewsResponse> PublishNews(IAsyncStreamReader<News> requestStream, ServerCallContext context)
@@ -27,6 +29,22 @@ namespace gSpaceServer.Services
       }
 
       return new NewsResponse { Delivered = true };
+    }
+
+    public override async Task MonitorSpace(Empty request, IServerStreamWriter<LineMessage> responseStream, ServerCallContext context)
+    {
+      var NewsList = new ReadOnlyCollection<string>(
+        new[]
+        {
+          "Yo Yo Yo - A state visit with high expectations",
+          "Yo Yo Yo - Election not a licence for violence",
+        });
+      
+      foreach(var news in NewsList)
+      {
+        await responseStream.WriteAsync(new LineMessage() { LinemessageItem = news, LinemessageTime = Timestamp.FromDateTime(DateTime.Now.ToUniversalTime())});
+        await Task.Delay(TimeSpan.FromSeconds(2));
+      }
     }
   }
 }
