aaa40a5bf09efbf8c9aff7efc5bb8655b875dec5
clad <clad@plex.com>
2023-06-21 09:13:57 +0530

    sample gRpc client

---
+++ gSpaceUser/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using gSpaceServer;
+
+namespace gSpaceUser;
+class Program
+{
+  static void Main(string[] args)
+  {
+    var channel = GrpcChannel.ForAddress("http://localhost:5078", new GrpcChannelOptions { UnsafeUseInsecureChannelCallCredentials = true });
+    var client = new Greeter.GreeterClient(channel);
+
+    var reply = client.SayHello(new HelloRequest { Name = "World!"});
+
+    Console.WriteLine(reply.Message);
+  }
+}
